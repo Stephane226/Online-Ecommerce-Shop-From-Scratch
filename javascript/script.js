5564a16f0c939e5b9701521b200360d7bfe558e2
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2021-02-27 08:06:11 +0300

    End of Features highlights design

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+//styling for the feature and highlight icons
+
+var featureHIcons = document.getElementsByClassName('icon-feature-items');
+for(i=0; i<featureHIcons.length; i++){
+  
+    featureHIcons[i].addEventListener("mouseover", function(event){
+      event.target.style.color = "white"
+    })
+    featureHIcons[i].addEventListener("mouseout", function(event){
+        event.target.style.color = "gray"
+      })
+}
+
