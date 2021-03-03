359e2d7f2f73560d75a4f1e45a7ddbe4fb0d603d
masaaki stephane <62558026+Stephane226@users.noreply.github.com>
2021-03-03 22:18:11 +0300

    products to basket functions

--- javascript/script.js
+++ javascript/script.js
@@ -26,7 +26,36 @@ for(b=0; b < addProductToBasketButton.length; b++){
 
 function addProductToBasket(event){
     var productParent = event.target.parentElement.parentElement;
-    console.log(productParent);
-    alert('clik')
+      
+    var images = productParent.getElementsByTagName('img')[0];
+     var productTitle = productParent.getElementsByClassName('product-title-name')[0];
+
+     var AddToCartRow = `
+        
+     <div class="one-shoping-element">
+                        <div class="one-shop-img">
+                            <img src="${images.src}" alt="">
+                        </div>
+                        <div class="one-shop-title-price">
+                            <h3>${productTitle}</h3>
+                            <span>$79</span>
+                        </div>
+                        <div class="one-shop-remove-numb">
+                             <span>  <i class="fas fa-times"></i> </span>
+                             <div class="numb-set">
+                                <i class="fas fa-angle-up"></i>
+                                  <span>2</span>
+                                   <i class="fas fa-angle-down"></i>
+                             </div>
+                        </div>
+                      </div>
+     
+     
+     `;
+     var div = document.createElement('div');
+     div.innerHTML = AddToCartRow;
+var shoppingCartUi = document.getElementById('shopping-card-parent-content');
+
+shoppingCartUi.appendChild(div)
 
 }
